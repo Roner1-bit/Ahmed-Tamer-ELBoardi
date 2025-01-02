9c24919e9fe503582d3a1b9933cd502f0720999d
Ahmed Tamer <58177688+Roner1-bit@users.noreply.github.com>
2025-01-02 22:04:30 +0900

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,44 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'LLM Researcher',
+    company: "Intelligent Media Labs, University of Hyogo",
+    duration: "(Oct 2024 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Machine Learning Engineer",
+    company: "Corelia",
+    duration: "(Jan 2023 - Sep 2024)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Research and Teaching Assistant",
+    company: "Nile University",
+    duration: "(Dec 2022 - Sep 2024)"
+  },
+  {
+    id: 4,
+    title: "Teaching Assistant (Part-time)",
+    company: "Ahram Canadian University",
+    duration: "(Oct 2022 - Dec 2022)"
+  },
+  {
+    id: 5,
+    title: "Backend Web Developer",
+    company: "Corelia",
+    duration: "(Sep 2022 - Dec 2022)"
+  },
+  {
+    id: 6,
+    title: "Android Developer (Part-time)",
+    company: "Jevils Marketing Agency",
+    duration: "(Mar 2021 - May 2021)"
+  },
+  {
+    id: 7,
+    title: "Internship",
+    company: "Pachin",
+    duration: "(Jan 2021 - Feb 2021)"
   }
-]
+];
